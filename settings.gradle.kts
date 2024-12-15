c288e2e3090139592a14cd0a8dd198832fa99538
Darwin Cruz <darwincruz68@yahoo.com>
2024-12-15 18:44:53 -0500

    appp

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.11.1" apply false
-        id("com.android.library") version "8.11.1" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+        id("com.android.application") version "8.7.3" apply false
+        id("com.android.library") version "8.7.3" apply false
+        id("org.jetbrains.kotlin.android") version "2.0.0" apply false
         id("com.google.devtools.ksp") version "1.9.22-1.0.17" // Reemplaza con la versión correcta si es necesario
     }
 }
