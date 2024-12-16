0e1482fc227b675233dd459feea63c09587e014c
Darwin Cruz <darwincruz68@yahoo.com>
2024-12-15 20:19:58 -0500

    apmto

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         id("com.android.application") version "8.7.3" apply false
         id("com.android.library") version "8.7.3" apply false
         id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-        id("com.google.devtools.ksp") version "1.9.22-1.0.17" // Reemplaza con la versión correcta si es necesario
+        id("com.google.devtools.ksp") version "2.0.0-1.0.22" // Reemplaza con la versión correcta si es necesario
     }
 }
 
